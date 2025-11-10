78bd0819ee2d6d1ce92ec2209a497dd527b0c09f
ifeomasylviadike <ifeomasylvia.dike@gmail.com>
2025-11-10 21:38:28 +0100

    Creating Specific Components in a React Application

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+function UserProfile() {
+    return (
+        <div>
+            <h2>Ifeoma Dike</h2>
+            <p>34 years</p>
+            <p>I am a Front End Developer</p>
+        </div>
+    );
+}
+
+export default UserProfile;
