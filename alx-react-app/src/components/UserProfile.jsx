152d2ed8bab3e4ae6b70580b27cf2d104c4b0be4
ifeomasylviadike <ifeomasylvia.dike@gmail.com>
2025-11-11 00:50:40 +0100

    react props

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile({ name, age, bio }) {
-  return (
+ return (
     <div>
-      <h2>{name}</h2>
-      <p>Age: {age}</p>
-      <p>{bio}</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 }
