013f9f7b240a4025ebdea58ccf38b5068d2ef9e2
ifeomasylviadike <ifeomasylvia.dike@gmail.com>
2025-11-12 03:58:29 +0100

    styling

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,6 +1,12 @@
 function UserProfile({ name, age, bio }) {
  return (
-    <div>
+    <div style={{ 
+      padding: '20px', 
+      margin: '20px',
+      border: '1px solid #ddd',
+      borderRadius: '8px',
+      backgroundColor: '#f9f9f9'
+    }}>
       <h2 style={{ color: 'blue' }}>{props.name}</h2>
       <p>Age: <span style={{ marginLeft: '10px' }}>{props.age}</span></p>
       <p>Bio: {props.bio}</p>
